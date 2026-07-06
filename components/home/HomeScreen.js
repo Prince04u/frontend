3072bf0abd813f38f0db2f392f662bbcca5cc2c2
Prince04u <2401030630043@silveroakuni.ac.in>
2026-07-06 12:54:23 +0530

    Initial commit

---
+++ components/home/HomeScreen.js
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import AppHeader from "./AppHeader";
+import PromoBanner from "./PromoBanner";
+import AnnouncementBar from "./AnnouncementBar";
+import CategoryTabs from "./CategoryTabs";
+import GameGrid from "./GameGrid";
+import PromoSection from "./PromoSection";
+import BottomNav from "./BottomNav";
+
+export default function HomeScreen() {
+  const [category, setCategory] = useState("all");
+
+  return (
+    <main className="club-app">
+      <AppHeader />
+      <PromoBanner />
+      <AnnouncementBar />
+      <CategoryTabs active={category} onChange={setCategory} />
+      <GameGrid category={category} />
+      <PromoSection />
+      <BottomNav />
+    </main>
+  );
+}
